bfedb30bca695804150cc5e276118cab19a3de5f
Cody <codylau6233@hotmail.com>
2023-11-16 16:24:19 -0800

    Collaborated with @Charlisteketee and @alyshajoy for this pair-programing assignment

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
@@ -32,6 +32,43 @@ describe("#wordSearch()", function() {
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
     ], 'SEINFELD')
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present", function() {
+    const result = wordSearch([
+      ['S', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['E', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['I', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['N', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['F', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['I', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['E', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['L', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['D', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SEINFIELD');
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if letters array is empty", () => {
+    const result = wordSearch([], 'Hello')
+    assert.isFalse(result);
+  });
+
+  it("should return positive if word is present, even if word was lowercase", () => {
+    const result = wordSearch([
+      ['S', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['E', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['I', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['N', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['F', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['I', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['E', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['L', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['D', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'seinfield');
 
     assert.isTrue(result);
   });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,37 @@
-const wordSearch = (letters, word) => {
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function(matrix) {
+  const result = [];
+  
+  for (let i = 0; i < matrix[0].length; i++) {
+    result.push([]);
+  }
+  for (let j = 0; j < matrix.length; j++) {
+    for (let k = 0; k < matrix[j].length; k++) {
+      result[k].push(matrix[j][k]);
     }
-}
-// test push
+  }
+  return result;
+};
+
+const wordSearch = (letters, word) => {
+  if (letters.length < 1) {
+    return false;
+  }
+  const upperCaseWord = word.toUpperCase();
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (const l of horizontalJoin) {
+    if (l.includes(upperCaseWord)) {
+      return true;
+    }
+  }
+
+  const transposed = transpose(letters);
+  const verticalJoin = transposed.map(ls => ls.join(''));
+  for (const l of verticalJoin) {
+    if (l.includes(upperCaseWord)) {
+      return true;
+    }
+  }
+  return false;
+};
+
 module.exports = wordSearch;
